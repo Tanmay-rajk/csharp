e591867f668d9976e5a7aa99452b2b901546d480
Tanmay R K <111168823+Tanmay-rajk@users.noreply.github.com>
2025-01-08 08:27:59 +0530

    Update program9.cs

--- program9.cs
+++ program9.cs
@@ -1,53 +1,115 @@
+// ############# Movement.cs
+usingusingusingSystem.Data;
+UnityEditor;
+UnityEngine;
+public class Movement : MonoBehaviour
+{
+Rigidbody2D rb;
+bool isGrounded = true;
+public float speed = 10f;
+public float jumpheight = 100f;
+public GameObject EndUI;
+// Start is called once before the first execution of Update after the MonoBehaviour is created
+void Start()
+{
+rb = GetComponent<Rigidbody2D>();
+Debug.Log("Start");
+EndUI.SetActive(false);
+}
+// Update is called once per frame
+void Update()
+{
+float x= Input.GetAxisRaw("Horizontal");
+if(x>0){
+rb.Velocity = new Vector2(speed*Time.deltaTime,rb.Velocity.y);
+}
+else if (x<0){
+rb.Velocity = new Vector2(-speed*Time.deltaTime,rb.Velocity.y);
+}
+if(Input.GetKeyDown(KeyCode.Space)&&isGrounded){
+rb.Velocity = new Vector2(rb.Velocity.x,jumpheight*Time.deltaTime);
+}
+}
+void OnTriggerEnter2D(Collider2D other){
+if(other.gameObject.tag == "Obstacle"){
+Destroy(gameObject);
+EndUI.SetActive(true);
+}
+}
+}
+// ################# Gamemanager.cs
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEditor.Overlays;
 using TMPro;
 public class Gamemanager : MonoBehaviour
 {
-public TextMeshProUGUI txt;
-int score=0;
-// Start is called before the first frame update
+int score =0;
+int old_score =10;
+string json;
+SaveDataval obj = new SaveDataval();
+public TextMeshProUGUI txt_old,txt_new;
+// Start is called once before the first execution of Update after the MonoBehaviour is created
 void Start()
 {
-txt.text=score.ToString();
+LoadData();
 }
-public void AddCoin(int val)
-{
-score=score+val;
+// Update is called once per frame
+public void AddCoin(int value){
+score = score +value;
 Debug.Log(score);
-txt.text=score.ToString();
+SaveGameData();
+txt_new.text = score.ToString();
 }
 }
-
-#################
-
-Step 1: Add Panel by Rightclick->UI
-Step 2: Add two ButtonMesh Pro and set it in Game
-Step 3: Name button as Quit and Restart
-Step 4 : Edit Gamemanager script to display UI and handle button
+void SaveGameData(){
+obj.score = score;
+json = JsonUtility.ToJson(obj);
+Debug.Log(json);
+File.WriteAllText(Application.dataPath+"Myfile.json",json);
+Debug.Log(Application.dataPath.ToString());
+}
+void LoadData(){
+string json = File.ReadAllText(Application.dataPath+"Myfile.json");
+obj = JsonUtility.FromJson<SaveDataval>(json);
+Debug.Log(old_score);
+txt_old.text = old_score.ToString();
+}
+class SaveDataval{
+public int score;
+}
+// ################ UIManager.cs
 using UnityEngine;
-using TMPro;
 using UnityEngine.SceneManagement;
-public class Gamemanager : MonoBehaviour
-{//code for score
-public TextMeshProUGUI txt;
-int score=0;
-// Start is called before the first frame update
-void Start()
+public class UIManager : MonoBehaviour
 {
-txt.text=score.ToString();
-}
-public void AddCoin(int val)
-{
-score=score+val;
-Debug.Log(score);
-txt.text=score.ToString();
-}
-//Code for button
-public void Restart()
-{
+// Start is called once before the first execution of Update after the MonoBehaviour is created
+public void Restart(){
 SceneManager.LoadScene(0);
 }
-public void Quit()
-{
+public void Quit(){
 Application.Quit();
+UnityEditor.EditorApplication.isPlaying=false;
+}
+}
+Coin.cs
+using UnityEngine;
+public class Playercoin : MonoBehaviour
+{
+int score = 10;
+Gamemanager gm;
+// Start is called once before the first execution of Update after the MonoBehaviour is created
+void Start()
+{
+gm = FindAnyObjectByType<Gamemanager>();
+}
+// Update is called once per frame
+void OnTriggerEnter2D(Collider2D other){
+if(other.tag == "Player"){
+gm.AddCoin(score);
+Destroy(gameObject);
+}
 }
 }
